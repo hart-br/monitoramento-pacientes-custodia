2b9cf3bb0d43ac2f42a57eaa10f95770258ffa98
hart-br <rafalvimsoares@gmail.com>
2026-05-21 11:27:42 -0300

    Update forms.py

--- forms.py
+++ forms.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pandas as pd
 from config import preenchimento_automatico
 
-opcoes_box = ["", "sim", "não"]
-
 class Forms:
     def __init__(self, box):
         self.data = ["Data", "data da internação", "data da alta médica", "data de envio do caso para juiz articulador",
@@ -12,22 +10,28 @@ class Forms:
         self.expand = ["Observações (sinalizar fatores como dificuldades de infraestrutura, de negativa de hospitais etc.)"]
         self.box = box
         self.especial = ["hospitais encaminhados"]
+        self.frase_1_hosp = "Qual o primeiro hospital que tentaram encaminhar o paciente?"
+        self.frase_hosp = "Qual foi o próximo hospital encaminhado?"
+        self.frase_aceito = "O paciente foi aceito no hospital?"
+        self.opcoes_box = opcoes_box = ["", "sim", "não"]
 
     def gerar_cols(self, cpf, df, existence):
         cols = [x for x in df.columns if x not in preenchimento_automatico]
-        linha = df[df["CPF"] == cpf]
+        linha = df[df["CPF"] == cpf].reset_index()
+        hospital_final = linha.at[0, "hospital final"]
         nova_linha = {}
 
         for col in [x for x in cols if x not in preenchimento_automatico]:
             st.write("")
             if col in self.box:
                 if col in self.especial:
-
+                    st.warning("Para cada hospital recusado, surgirá um novo campo para preencher o próximo hospital")
                     hospitais = []
 
+                    #Se já existir hospitais com ou sem recusa
                     if existence and pd.notna(linha.iloc[0][col]):
                         valor_atual = linha.iloc[0][col]
-                        hospitais_cp = [x for x in valor_atual.split(", ")]
+                        hospitais_cp = [x.split() for x in valor_atual.split(", ") if x.split() != "-"]
 
                         #Placeholders para controlar iterações
                         n_hospital = 300 #valor aleatorio so para distinguir do n_aceito
@@ -37,46 +41,46 @@ class Forms:
                             n_hospital += 1
                             n_aceito += 1
 
-                            if i == len(hospitais_cp) -1:
-                                hospitais.append(
-                                    st.selectbox("Qual foi o próximo hospital encaminhado?",
-                                                 self.box[col], index=self.box[col].index(hospitais_cp[i]), key=n_hospital))
-                                aceito = st.selectbox("O paciente foi aceito no hospital?", opcoes_box, key=n_aceito)
-                                fim = aceito != "não"
-                                st.write("")
-
-                            elif i == 0:
-                                hospitais.append(st.selectbox("Qual o primeiro hospital que o paciente foi encaminhado?",
-                                                              self.box[col], index=self.box[col].index(hospitais_cp[i]), key=n_hospital))
-                                aceito = st.selectbox("O paciente foi aceito no hospital?", opcoes_box, index=2, key=n_aceito)
-                                st.write("")
-
+                            #Pergunta do hospital
+                            if i == 0:
+                                hospitais.append(st.selectbox(self.frase_1_hosp, self.box[col],
+                                                              index=self.box[col].index(hospitais_cp[i]), key=n_hospital))
                             else:
-                                hospitais.append(st.selectbox("Qual foi o próximo hospital encaminhado?",
-                                                 self.box[col], index=self.box[col].index(hospitais_cp[i]), key=n_hospital))
-                                aceito = st.selectbox("O paciente foi aceito no hospital?", opcoes_box, index=2, key=n_aceito)
-                                st.write("")
+                                hospitais.append(st.selectbox(self.frase_hosp, self.box[col],
+                                                              index=self.box[col].index(hospitais_cp[i]), key=n_hospital))
+                            st.write("")
 
+                            #Pergunta do aceite
+                            if hospital_cp == hospital_final:
+                                aceito = st.selectbox(self.frase_aceito, self.opcoes_box, index=1, key=n_aceito)
+                                fim = aceito != "não"
+                            elif i < len(hospitais_cp) - 1:
+                                aceito = st.selectbox(self.frase_aceito, self.opcoes_box, index=2, key=n_aceito)
+                            else:
+                                aceito = st.selectbox(self.frase_aceito, self.opcoes_box, key=n_aceito)
+                                fim = aceito != "não"
+                            st.write("")
 
+                    #Se não existir hospitais cadastrados ainda
                     else:
                         n_hospital = 300
                         n_aceito = 0
 
-                        hospitais.append(st.selectbox("Qual o primeiro hospital que o paciente foi encaminhado?", self.box[col], key=n_hospital))
-                        aceito = st.selectbox("O paciente foi aceito no hospital?", opcoes_box, key=n_aceito)
+                        hospitais.append(st.selectbox(self.frase_1_hosp, self.box[col], key=n_hospital))
+                        aceito = st.selectbox(self.frase_aceito, self.opcoes_box, key=n_aceito)
                         fim = aceito != "não"
                         st.write("")
 
                     while not fim:
                         n_hospital += 1
                         n_aceito += 1
-                        hospitais.append(st.selectbox("Qual foi o próximo hospital encaminhado?", self.box[col], key=n_hospital))
-                        aceito = st.selectbox("O paciente foi aceito no hospital?", opcoes_box, key=n_aceito)
+                        hospitais.append(st.selectbox(self.frase_hosp, self.box[col], key=n_hospital))
+                        aceito = st.selectbox(self.frase_aceito, self.opcoes_box, key=n_aceito)
                         fim = aceito != "não"
                         st.write("")
 
-                    nova_linha[col] = ", ".join(hospitais)
-
+                    nova_linha[col] = ", ".join([x.split() for x in hospitais if x.split() != "-"])
+                    st.write("")
 
                 elif existence and pd.notna(linha.iloc[0][col]):
                     valor_atual = linha.iloc[0][col]
